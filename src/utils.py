9b098cb8f707001c72be6bf5cea74ff2f0b94c4e
bimal-bp <bimalpatrap@gmail.com>
2024-02-12 16:36:15 +0530

    Model training complete

--- src/utils.py
+++ src/utils.py
@@ -15,3 +15,32 @@ def save_object(filepath,obj):
             dill.dump(obj,file_obj)
     except Exception as e:
         raise SensorException(e,sys)
+
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
+    try:
+        report={}
+        for i in range(len(list(models()))):
+            model=list(models.values())[i]
+            para=param[list(model.keys())][i]
+
+            gs=GridSearchCV(model,para,cv=2)
+            gs.fit(X_train,y_train)
+
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
+
+            train_model_score=r2_score(y_train,y_train_pred)
+            test_model_score=r2_score(y_test,y_test_pred)
+            report[list(models.keys()[i])]=test_model_score
+
+        return report 
+    except Exception as e:
+        raise SensorException(e,sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise SensorException(e,sys)
